7b56f724f6c08d9c3ba2ee1def919fd0620d1e5a
LeoBC24 <s2401553@edu.bc.fi>
2025-04-24 10:14:00 +0300

    update v3

--- index.js
+++ index.js
@@ -98,7 +98,9 @@ app.post('/api/persons', async (req, res, next) => {
 });
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
+  console.error('Error name:', error.name);
+  console.error('Error message:', error.message);
+  console.error('Error stack:', error.stack);
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' });
@@ -106,8 +108,9 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).json({ error: error.message });
   }
 
-  next(error);
+  return res.status(500).json({ error: 'Internal server error' });
 };
+
 
 app.use(errorHandler);
 
