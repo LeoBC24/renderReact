7188b8b77fedabe943f951d7a1f953c87af344b1
LeoBC24 <s2401553@edu.bc.fi>
2025-04-24 13:37:51 +0300

    update v6

--- index.js
+++ index.js
@@ -142,9 +142,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-app.use(errorHandler);
